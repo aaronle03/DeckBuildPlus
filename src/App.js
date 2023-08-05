a9ecde21fd16b53dc2b06dd0de10ac37f7bf2a11
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-08-04 20:35:48 -0700

    changed server to mongo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../src/components/Navbar';
+import Builder from '../src/components/Builder';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Builder></Builder>
+    </div>
+  )
+}
+
+export default App
