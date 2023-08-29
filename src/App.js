c0f229e276929b5c04ffba34f67203034dd1e304
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-08-29 15:11:45 -0700

    finished fundamentals, need to add view personal decks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Builder from './pages/Builder';
-import CardIndex from './pages/CardIndex';
-import PersonalDeck from './pages/PersonalDeck';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Builder />} />
-          <Route path="/CardIndex" element={<CardIndex />} />
-          <Route path="/PersonalDeck" element={<PersonalDeck />} />
-        </Routes>
-      </div>
-    </Router>
+    <div class="app-container">
+      <Navbar></Navbar>
+      <Builder></Builder>
+    </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="nav">
-      <section className='logo'>
-        <section className='logo-title'>
-          <h1>DECKBUILD+</h1>
-          <h3>Marval Snap Deck Builder</h3>
-        </section>
-      </section>
-      <section className='navbar-links'>
-        <Link to="/">BUILDER ⚙️</Link>
-        <Link to="/PersonalDeck">MY DECKS 🔥</Link>
-        <Link to="/CardIndex">CARD INDEX 📖</Link>
-      </section>
-      <section>
-      </section>
-    </nav>
+          <section className='logo'>
+            <section className='logo-title'>
+              <h1>DECKBUILD+</h1>
+              <h3>Marval Snap Deck Builder</h3>
+            </section>
+          </section>
+          <section className='navbar-links'>
+                <a href="/">BUILDER ⚙️</a>
+                <a href="https://www.linkedin.com/in/aaronctle/">CREATED BY AARON LE 🔥</a>
+          </section>
+          <section>
+          </section>
+      </nav>
   )
 }
 
-export default Navbar;
+export default Navbar
